7e020170e795d4259f92d7c14dfad39205310efe
xRichO7 <d00280247@student.dkit.ie>
2025-10-23 22:19:16 +0100

    Add SQL delete queries for members and coaches
    Added SQL queries to delete members without coaches, coaches with less than 2 years of experience, and participants who did not finish.

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,1 +1,17 @@
--- Sql QUeries go here
+-- SQL Delete
+
+-- No assigned coaches & aren't in competition records
+DELETE FROM MEMBERS
+WHERE Coach_ID IS NULL
+  AND Member_ID NOT IN (
+      SELECT Member_ID 
+      FROM COMPETITION_PARTICIPANTS
+  );
+
+-- Delete for coaches with less than 2yr experience
+DELETE FROM COACHES
+WHERE Years_Exerperience < 2;
+
+--Delete participants who DNF
+DELETE FROM COMPETION_PARTICIPANTS
+WHERE PLACEMENT IS NULL;
