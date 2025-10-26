86b32a69d8cb61f67a4a9b3c67988a20381938e1
conormurphyy <D00279403@student.dkit.ie>
2025-10-26 15:20:04 +0000

    Update triggers.sql

--- SQL/Triggers/triggers.sql
+++ SQL/Triggers/triggers.sql
@@ -1,8 +1,23 @@
 -- Trigger doesn't allow people under the age of 13 to be able to become a member
 -- https://www.ibm.com/docs/en/db2/11.5.x?topic=statements-signal reference to signal sql state.
+-- Trgger for insertting invalid ages
 DELIMITER $$
 CREATE TRIGGER check_age
 BEFORE INSERT ON MEMBERS
+FOR EACH ROW
+BEGIN
+    -- Check the members  age before inserting
+    IF NEW.Age < 13 THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Member must be at least 13 years old.';
+    END IF;
+END$$
+DELIMITER ;
+
+--Trigger for updating invalid ages
+DELIMITER $$
+CREATE TRIGGER check_age
+BEFORE UPDATE ON MEMBERS
 FOR EACH ROW
 BEGIN
     -- Check the members  age before inserting
